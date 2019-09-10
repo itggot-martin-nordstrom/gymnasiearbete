b68b5ebd7511278926c41c7fed6fcd1ac77fd4c9
itggot-martin-nordstrom <martin.nordstrom2@elev.ga.ntig.se>
2019-09-10 13:14:49 +0200

    testing various small function
    only for startup

---
+++ various_functions_test.rb
@@ -0,0 +1,13 @@
+def win_calculator(rating1, rating2)
+    # MAGIC
+end
+
+
+
+def correct_percentage(no_of_correct, no_of_incorrect)
+    total = (no_of_correct + no_of_incorrect) * 1.0
+
+    return no_of_correct/total
+end
+
+p correct_percentage(80, 43)
